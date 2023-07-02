c6aae1339cf714f30ff02d23dee25bbd5406e2aa
Kanaparthy Sai Sreekar <kanapasai@gmail.com>
2023-07-02 12:54:12 +0530

    2

--- src/Components/Attack.js
+++ src/Components/Attack.js
@@ -10,13 +10,17 @@ function Attack() {
         }, 18000)
     }
     return (
-        <div className='container border rounded p-0 mt-5'>
-            <div className='row align-items-center mr-4'>
-                <h1 className='col-9 display-6 p-2 ms-3'>Attack</h1>
-                    <a className='col-1 btn btn-primary p-2 btn-sm' onClick={clickHandler} type='button'>Click Me</a>
+            <div className="container border rounded p-1 mt-5">
+              <div className="row align-items-center">
+                <h1 className="col display-6 p-2 ms-3 me-auto col-12 col-sm-auto ps-4">Attack</h1>
+                <div className="col-12 col-md-3 d-flex justify-content-end">
+                  <a className="btn btn-primary btn-sm" onClick={clickHandler} type="button">
+                    Click Me
+                  </a>
+                </div>
                 {trigger && <Timer />}
+              </div>
             </div>
-        </div>
-    )
-}
+          );
+        }
 export default Attack;
--- src/Components/Defence.js
+++ src/Components/Defence.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 function Defence() {
     return (
-        <div className='container border rounded p-0 mt-3'>
-            <div className='row align-items-center mr-4'>
-                <h1 className='col-9 display-6 p-2 ms-3'>Defence</h1>
-                <a className='col-1 btn btn-primary p-2 btn-sm' type='button'>Click Me</a>
-            </div>
+        <div className="container border rounded p-1 mt-2">
+        <div className="row align-items-center">
+          <h1 className="col display-6 p-2 ms-3 me-auto col-12 col-sm-auto ps-4">Defence</h1>
+          <div className="col-12 col-md-3 d-flex justify-content-end">
+            <a className="btn btn-primary btn-sm" type="button">
+              Click Me
+            </a>
+          </div>
         </div>
-    )
-}
+      </div>
+          );
+        }
 export default Defence;
