7963866ac7654b69b4e147b850a2ac5dcd7e38d9
Kanaparthy Sai Sreekar <kanapasai@gmail.com>
2023-07-02 18:57:59 +0530

    3

--- src/Components/Attack.js
+++ src/Components/Attack.js
@@ -1,48 +1,51 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
 import Timer from './timer';
+
 function Attack() {
-    const [trigger, setTrigger] = useState(false);
-    const [attacked, setAttacked] = useState(false);
-    async function starts() {
-        console.log("A")
-        setTrigger(true);
-        setTimeout(() => {
-            setTrigger(false);
-        }, 18000)
-        const Response = await fetch("http://13.127.3.114:8080/connect", {
-            crossDomain: true,
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-            },
-        })
+  const [trigger, setTrigger] = useState(false);
+  const [attacked, setAttacked] = useState(false);
 
-        const dat = await Response.json();
-        console.log(dat)
-        const newTab = window.open(dat.destination_url, '_blank');
-        setTrigger(false);
-        setAttacked(true)
-        // newTab.focus();
+  async function starts() {
+    console.log("A")
+    setTrigger(true);
+    setTimeout(() => {
+      setTrigger(false);
+    }, 18000);
+
+    try {
+      const response = await fetch("http://13.127.3.114:8080/connect", {
+        crossDomain: true,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+
+      const data = await response.json();
+      console.log(data);
+      const newTab = window.open(data.destination_url, '_blank');
+      setTrigger(false);
+      setAttacked(true);
+      // newTab.focus();
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    // const clickHandler = () => {
-    //     setTrigger(true);
-    //     setTimeout(() => {
-    //         setTrigger(false);
-    //     }, 18000)
-    // }
-    return (
-        <div className="container border rounded p-1 mt-5">
-            <div className="row align-items-center">
-                <h1 className="col display-6 p-2 ms-3 me-auto col-12 col-sm-auto ps-4">Attack</h1>
-                <div className="col-12 col-md-3 d-flex justify-content-end">
-                    <button className="btn btn-primary btn-sm" onClick={starts} disabled={attacked}>
-                        Click Me
-                    </button>
-                </div>
-                {trigger && <Timer />}
-            </div>
+  return (
+    <div className="container border rounded p-4 mt-5">
+      <h1 className="display-6 text-center">Attack</h1>
+      <div className="row justify-content-center mt-4">
+        <div className="col-auto">
+          <button className="btn btn-primary" onClick={starts} disabled={attacked}>
+            Click Me
+          </button>
         </div>
-    );
+      </div>
+      <div className="row justify-content-center mt-4">
+        {trigger && <Timer />}
+      </div>
+    </div>
+  );
 }
+
 export default Attack;
--- src/Components/Defence.js
+++ src/Components/Defence.js
@@ -1,96 +1,105 @@
 import React, { useState } from 'react';
 import Timer from './timer';
+
 function Defence() {
-    const [trigger, setTrigger] = useState(false);
-    const [Downloaded, setDownloaded] = useState(false);
-    const [Uploaded, setUploaded] = useState(false);
-    async function starts() {
-        setTrigger(true);
-        try {
-            const Response = await fetch("http://65.1.91.14:8085/connect", {
-                crossDomain: true,
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                },
-            });
-            if (Response.ok) {
-                console.log("Fetch success");
-            }
-            else {
-                console.log("Fetch Failed");
-            }
-            const dat = await Response.json();
-            console.log(dat.destination_url);
-            const newTab = window.open(dat.destination_url, '_blank');
-        }
-        catch (e) {
-            console.log(e);
-        }
-        finally {
-            setTrigger(false);
-        }
-        // newTab.focus();
-    }
-    async function uploadFile(file) {
-        const formData = new FormData();
-        formData.append('file', file);
+  const [trigger, setTrigger] = useState(false);
+  const [downloaded, setDownloaded] = useState(false);
+  const [uploaded, setUploaded] = useState(false);
 
-        try {
-            const Response = await fetch('http://13.232.174.98:8088/upload', {
-                method: 'POST',
-                body: formData,
-            });
-            const dat = await Response.json();
-            console.log(dat)
-            if (Response.ok) {
-                console.log('File uploaded successfully!');
-                setUploaded(true)
-            } else {
-                console.log('Error uploading file');
-            }
-        } catch (error) {
-            console.log('An error occurred:', error);
-        }
-    }
-    const handleUploadClick = (event) => {
-        const file = event.target.files[0];
-        console.log(file)
-        if (file) {
-            uploadFile(file);
-            // setTrigger(true);
-        }
-    }
-    const handleDownloadClick = (event) => {
-        setDownloaded(true);
-    }
-    return (
-        <div className="container border rounded p-1 mt-2">
-            <div className="row align-items-center">
-                <h1 className="col display-6 p-2 ms-3 me-auto col-12 col-sm-auto ps-4">Defence</h1>
-                <div className="col-12 col-md-3 d-flex justify-content-end">
+  async function starts() {
+    setTrigger(true);
 
-                    <a href='http://65.1.3.154:8089/download' className="btn btn-primary me-2 btn-sm" onClick={handleDownloadClick} type="button">
-                        Download File
-                    </a>
-                    <input
-                        type="file"
-                        accept=".js,.java"
-                        style={{ display: 'none' }}
-                        id="file-upload-input"
-                        onChange={handleUploadClick}
-                        disabled={!Downloaded}
-                    />
-                    <label htmlFor="file-upload-input" disabled={!Downloaded} type="button" className="btn btn-primary me-2 btn-sm">
-                        Upload File
-                    </label>
-                    <button className="btn btn-primary btn-sm" disabled={!Uploaded} onClick={starts} type="button">
-                        Click Me
-                    </button>
+    try {
+      const response = await fetch("http://65.1.91.14:8085/connect", {
+        crossDomain: true,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
 
-                </div>
-                {trigger && <Timer />}
-            </div>
+      if (response.ok) {
+        console.log("Fetch success");
+      } else {
+        console.log("Fetch Failed");
+      }
+
+      const data = await response.json();
+      console.log(data.destination_url);
+      const newTab = window.open(data.destination_url, '_blank');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setTrigger(false);
+    }
+    // newTab.focus();
+  }
+
+  async function uploadFile(file) {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await fetch('http://13.232.174.98:8088/upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (response.ok) {
+        console.log('File uploaded successfully!');
+        setUploaded(true);
+      } else {
+        console.log('Error uploading file');
+      }
+    } catch (error) {
+      console.log('An error occurred:', error);
+    }
+  }
+
+  const handleUploadClick = (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+
+    if (file) {
+      uploadFile(file);
+    }
+  };
+
+  const handleDownloadClick = (event) => {
+    setDownloaded(true);
+  };
+
+  return (
+    <div className="container border rounded p-4 mt-2">
+      <h1 className="display-6 text-center">Defence</h1>
+      <div className="row justify-content-center mt-4">
+        <div className="col-auto">
+          <a href="http://65.1.3.154:8089/download" className="btn btn-primary me-2" onClick={handleDownloadClick} disabled={!downloaded}>
+            Download File
+          </a>
+          <label htmlFor="file-upload-input" className="btn btn-primary me-2" disabled={!downloaded}>
+            Upload File
+            <input
+              type="file"
+              accept=".js,.java"
+              style={{ display: 'none' }}
+              id="file-upload-input"
+              onChange={handleUploadClick}
+              disabled={!downloaded}
+            />
+          </label>
+          <button className="btn btn-primary" disabled={!uploaded} onClick={starts}>
+            Click Me
+          </button>
         </div>
-    );
+      </div>
+      <div className="row justify-content-center mt-4">
+        {trigger && <Timer />}
+      </div>
+    </div>
+  );
 }
+
 export default Defence;
