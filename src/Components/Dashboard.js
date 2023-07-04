3dc207bc77372bea0fb97c6f57ff781d60df8391
Preet <sardapreet@gmail.com>
2023-07-04 09:45:11 +0530

    final

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,14 +4,13 @@ import { Link } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div>
-            <h1 className="my-4 text-center">SQL Injection</h1>
+            <h1 className="display-1 p-5 text-center">SQL Injection</h1>
             <Row className="card-container">
                 <Row>
                     <Card className="my-2">
                         <Card.Body>
                             <Card.Title>Lab 1</Card.Title>
-                            <Card.Text>content</Card.Text>
-                            <Link className='btn btn-primary' type='button' to="/lab" >Click Me</Link>
+                            <Link className='btn btn-primary mt-3' type='button' to="/lab" >Click Me</Link>
                         </Card.Body>
                     </Card>
                 </Row>
